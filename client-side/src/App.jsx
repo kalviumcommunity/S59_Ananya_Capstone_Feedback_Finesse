163a83bc3d2411b5a2604baa1e836104d5647cdd
ananyatewari <ananyatewari0205@gmail.com>
2024-04-01 16:45:44 +0530

    made changes as asked

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import PulseLoader from "react-spinners/PulseLoader";
 
+const colour = "#D0312D"
+const size = 20
+
 export default function App() {
   return (
     <>
       <div className="logo">
         Feedback Finesse
-        <PulseLoader color="#D0312D" size={20} />
+        <PulseLoader color={colour} size={size} />
       </div>
     </>
   );
