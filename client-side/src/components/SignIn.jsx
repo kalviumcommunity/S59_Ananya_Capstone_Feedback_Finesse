4cf7dc0a2adba7518a6c201c20f091563f16a52d
ananyatewari <ananyatewari0205@gmail.com>
2024-04-15 18:40:37 +0530

    added page

--- client-side/src/components/SignIn.jsx
+++ client-side/src/components/SignIn.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function SignIn() {
   return (
-    <div>SignIn</div>
+    <>
+    <section className='in-parent'>
+      <section id='signin'>
+        <h3>Sign in to your account</h3>
+      </section>
+    </section>
+    </>
   )
 }
 
