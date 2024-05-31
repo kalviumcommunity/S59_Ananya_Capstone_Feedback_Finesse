30c8b89bcd00309ae32ac8756dd11b713d3a3c12
ananyatewari <ananyatewari0205@gmail.com>
2024-05-31 22:52:43 +0530

    added/made changes to css of dash

--- client-side/src/dash/AllTickets.jsx
+++ client-side/src/dash/AllTickets.jsx
@@ -16,6 +16,19 @@ import ConfirmUpdate from "./ConfirmUpdate";
 
 function AllTickets() {
 
+  const getStatusStep = (status) => {
+    switch (status) {
+      case "Submitted":
+        return 0;
+      case "In Progress":
+        return 1;
+      case "Resolved":
+        return 2;
+      default:
+        return 0;
+    }
+  };
+
   const [complaintData, setComplaintData] = useState({
     title: "",
     content: "",
@@ -39,12 +52,6 @@ function AllTickets() {
   const [updateID, setUpdateID] = useState(null)
   const [toSend, setToSend] = useState(null)
   const [uploading, setUploading] = useState(null);
-
-  const statusSteps = ["Submitted", "In Progress", "Resolved"];
-
-  const getStatusStep = (status) => {
-    return statusSteps.indexOf(status);
-  };
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_URI}/complaint/viewpost`)
@@ -155,23 +162,23 @@ function AllTickets() {
     }
   };
 
-  const handleLeft = (fileid, totalImages) => {
+  const handleLeft = (fileid) => {
     const postIndex = post.findIndex((e) => e._id === fileid);
     if (postIndex !== -1) {
       setImageIndex((prevIndexes) => {
         const updatedIndexes = [...prevIndexes];
-        updatedIndexes[postIndex] = updatedIndexes[postIndex] === 0 ? totalImages - 1 : updatedIndexes[postIndex] - 1;
+        updatedIndexes[postIndex] = Math.max(updatedIndexes[postIndex] - 4, 0);
         return updatedIndexes;
       });
     }
   };
-  
+
   const handleRight = (fileid, totalImages) => {
     const postIndex = post.findIndex((e) => e._id === fileid);
     if (postIndex !== -1) {
       setImageIndex((prevIndexes) => {
         const updatedIndexes = [...prevIndexes];
-        updatedIndexes[postIndex] = updatedIndexes[postIndex] === totalImages - 1 ? 0 : updatedIndexes[postIndex] + 1;
+        updatedIndexes[postIndex] = Math.min(updatedIndexes[postIndex] + 4, totalImages);
         return updatedIndexes;
       });
     }
@@ -203,105 +210,128 @@ function AllTickets() {
 
   return (
     <>
-      <section className="main">
-        <div className="dashboard-parent">
-          <Dashboard />
-        </div>
-        <div className="main-child flex flex-col justify-center items-center w-full">
-          <button
-            id="goback-toview"
-            className="publish text-sm"
-            onClick={() => setShowMakePost(!showMakePost)}
-          >
-            {showMakePost ? (
-              <h2>
-                <i className="bx bxs-arrow-to-left"></i> Go back to viewing all
-                posts
-              </h2>
-            ) : (
-              "Click to make a post"
-            )}
-          </button>
-
+    <section className="main">
+      <div className="dashboard-parent">
+      <Dashboard />
+      </div>
+      <div className="main-child flex flex-col justify-center items-center w-full">
+        <button
+          id="goback-toview"
+          className="publish text-sm"
+          onClick={() => setShowMakePost(!showMakePost)}
+        >
           {showMakePost ? (
-            <MakePost
-              setImageUpload={setImageUpload}
-              uploadImage={uploadImage}
-              imageUpload={imageUpload}
-              handleSubmit={handleSubmit}
-              handleChange={handleChange}
-              complaintData={complaintData}
-              uploading={uploading}
-            />
-          ) : (
-            <>
-              <div className="allpost">
-                {Array.isArray(post) && post.map((file, index) => (
-                  <div id="eachpost" key={index} className="flex flex-row justify-between">
+            <h2>
+              <i className="bx bxs-arrow-to-left"></i> Go back to viewing all
+              posts
+            </h2>
+           ) : (
+            "Click to make a post"
+          )}
+        </button>
 
-                  <span className="flex flex-row justify-between">
-                    
-                    <span className="flex flex-col justify-start">
-                      <h3 className="text-darkred font-bold flex flex-row items-center user"> <img style={{height: "8vh", width: "8vh"}} src={user} alt="" className="mr-3" /> <span>{file.username}</span></h3>
-                      <hr style={{backgroundColor: "black", height: "0.2vh", width: "fit-content"}} className="mb-4" />
-                      <h3 className="flex flex-row items-center"><i className='bx bxs-id-card mr-1 text-3xl' style={{color: "#900000"}}></i>University ID: <span className="ml-2">{file.universityID}</span></h3>
-                      <h3 className="flex flex-row items-center"><i className='bx bxs-building-house mr-1 text-3xl' style={{color: "#900000"}}></i>Hostel: <span className="ml-2">{file.hostel}</span></h3>
-                      <h3>Title: <span>{file.title}</span></h3>
-                    </span>
+      {showMakePost ? (
+        <MakePost
+          setImageUpload={setImageUpload}
+          uploadImage={uploadImage}
+          imageUpload={imageUpload}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          complaintData={complaintData}
+          uploading={uploading}
+       />
+       ) : (
+        <>
+         <div className="allpost">
+         {Array.isArray(post) && post.map((file, index) => (
+          <div className="eachpost" key={file._id}>
 
-                    <span>
-                    {file.picture && file.picture.length > 0 && (
-                      <>
-                      <h5 className="text-center text-sm mt-2 mb-2">{imageIndex[index] + 1} of {file.picture.length} images</h5>
-                      <div className="flex flex-row space-evenly items-center justify-center">
-                        <ArrowBackIosIcon className={file.picture.length > 1 ? "workingarrow" : "disablearrow"} onClick={() => handleLeft(file._id, file.picture.length)}  />
-                        <img src={file.picture[imageIndex[index]]} alt="" />
-                        <ArrowForwardIosIcon className={file.picture.length > 1 ? "workingarrow" : "disablearrow"} onClick={() => handleRight(file._id, file.picture.length)}  />
-                      </div>
-                    {confirmDelete ? <ConfirmDelete deletePost={deletePost} setConfirmDelete={setConfirmDelete} setDeletePost={setDeletePost} /> : null}
-
-                    {updatePost ? <ConfirmUpdate updateID={updateID} setUpdateID={setUpdateID} setUpdatePost={setUpdatePost} toSend={toSend} complaintData={complaintData} setComplaintData={setComplaintData} setPost={setPost} /> : null}
-                    <div className="progress-tracker">
-                      {statusSteps.map((step, stepIndex) => (
-                        <div key={step} className={`progress-step ${stepIndex <= getStatusStep(file.status) ? 'active' : ''}`}>
-                          <div className="progress-icon">{stepIndex + 1}</div>
-                          <p>{step}</p>
-                        </div>
-                      ))}
-                    </div>
-                      </>
-                    )}
-
-                    </span>
-                  </span>
-
-                  <span>
-                    <h3 className="mt-2">Description: <br /> <span>{file.content}</span></h3>
-                  </span>
-                    <hr style={{width: "100%", height: "0.2vh", backgroundColor: "#999900"}} className="mt-8 mb-1"/>
-
-                  <span className="flex flex-row justify-between mt-3" id="last-buttons">
-                  <div onClick={() => toggleLike(file._id)}>
-                    {!likes[file._id] ? (
-                      <i className='bx bx-like text-5xl text-darkred'></i>
-                    ) : (
-                      <i className='bx bxs-like text-5xl text-darkred'></i>
-                    )}
-                  </div>
-                      <div className="flex flex-row items-center gap-4 mr-8">
-                        {sessionStorage.getItem("role") == "admin" || file.username == sessionStorage.getItem("username") ? 
-                        <>
-                        <button className="flex flex-row items-center" onClick={() => handleeditpost(file._id)} >Edit <img src={edit} alt="edit" className="ml-1"/> </button>
-                        <button className="flex flex-row items-center" onClick={() => handledeletePost(file._id)}>Delete <img src={del} alt="delete" className="ml-1"/></button>
-                        </>
-                      : null}
-                      </div>
-                    </span>
-                    
-                  </div>
-                ))}
+            <div className="firstrow flex flex-row items-center justify-between">
+              <div><h3 className="text-darkred font-bold flex flex-row items-center user"> <img style={{height: "8vh", width: "8vh", boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.6)", borderRadius: "50%"}} src={user} alt="" className="mr-3" />{file.username}</h3></div>
+              <div className="progress-tracker flex flex-row">
+                status of complaint
               </div>
-            </>
+            </div>
+
+            <div className="secondrow">
+              <h3 className="flex items-center"><i className='bx bxs-id-card mr-1 text-3xl' style={{color: "#900000"}}></i>University ID: <span>{file.universityID}</span></h3>
+              <h3 className="flex items-center"><i className='bx bxs-building-house mr-1 text-3xl' style={{color: "#900000"}}></i>Hostel: <span>{file.hostel}</span></h3>
+              <h3 className="flex items-center"><i className='bx bxs-book-open mr-1 text-3xl' style={{color: "#900000"}}></i>Issue: <span>{file.title}</span></h3>
+            </div>
+
+            <div className="thirdrow">
+              <span className="ml-4">
+              <h3 className="font-bold underline mb-2">Description: </h3>
+              <span>
+              {file.content}
+              </span>
+              </span>
+            {/* </div> */}
+
+            {/* <div className="fourthrow image-slider"> */}
+              {file.picture && file.picture.length > 0 && (
+                <div className="image-slider-controls flex flex-col w-fit">
+                  <div className="flex items-center justify-between">
+                    <div>
+                    <ArrowBackIosIcon
+                      className={imageIndex[index] > 0 ? "workingarrow" : "disablearrow"}
+                      onClick={() => handleLeft(file._id)}
+                      />
+                    </div>
+                    <div className="image-slider-content flex">
+                      {file.picture
+                        .slice(imageIndex[index], imageIndex[index] + 4)
+                        .map((image, imgIndex) => (
+                          <img key={imgIndex} src={image} alt={`img-${imgIndex}`} />
+                        ))}
+                    </div>
+                    <div>
+                    <ArrowForwardIosIcon
+                      className={
+                        imageIndex[index] + 4 < file.picture.length
+                        ? "workingarrow"
+                        : "disablearrow"
+                      }
+                      onClick={() => handleRight(file._id, file.picture.length)}
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <h5 className="text-sm mt-2 text-center">
+                      {Math.min(imageIndex[index] + 4, file.picture.length)} of {file.picture.length} image(s)
+                    </h5>
+                  </div>
+                </div>
+              )}
+            </div>
+
+          {confirmDelete ? <ConfirmDelete deletePost={deletePost} setConfirmDelete={setConfirmDelete} setDeletePost={setDeletePost} /> : null}
+          {updatePost ? <ConfirmUpdate updateID={updateID} setUpdateID={setUpdateID} setUpdatePost={setUpdatePost} toSend={toSend} complaintData={complaintData} setComplaintData={setComplaintData} setPost={setPost} /> : null}
+            
+            <hr className="fifthrow" />
+            <div className="fifthrow">
+            <span className="flex flex-row justify-between mt-3" id="last-buttons">
+                   <div onClick={() => toggleLike(file._id)}>
+                     {!likes[file._id] ? (
+                      <i className='bx bx-like text-5xl text-darkred ml-5'></i>
+                    ) : (
+                      <i className='bx bxs-like text-5xl text-darkred ml-5'></i>
+                    )}
+                  </div>
+                    <div className="flex flex-row items-center gap-4 mr-8">
+                      {sessionStorage.getItem("role") == "admin" || file.username == sessionStorage.getItem("username") ? 
+                      <>
+                      <button className="flex flex-row items-center" onClick={() => handleeditpost(file._id)} >Edit <img src={edit} alt="edit" className="ml-1"/> </button>
+                      <button className="flex flex-row items-center" onClick={() => handledeletePost(file._id)}>Delete <img src={del} alt="delete" className="ml-1"/></button>
+                      </>
+                      : null}
+                    </div>
+                  </span>
+              </div>
+            </div>         
+          ))}
+          </div>
+        </>
           )}   
         </div>
       </section>
